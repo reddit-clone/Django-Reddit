0c4f7108595775232ec6a660f425f11136fa47d6
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-04-12 14:04:09 -0400

    Second sprint models, updated settings.py for database

--- django_reddit/django_reddit/settings.py
+++ django_reddit/django_reddit/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_reddit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reddit_django,
+        'USER': 'reddit',
+        'PASSWORD': 'django',
+        'HOST': 'localhost'
     }
 }
 
