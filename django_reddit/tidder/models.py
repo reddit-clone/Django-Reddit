a5f012e7eaa44fe20400a9d41d1c93d212758ab5
Seth Mcbay <sethmcbay@yahoo.com>
2019-04-12 11:40:11 -0400

    started models

--- django_reddit/tidder/models.py
+++ django_reddit/tidder/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Post (models.Model):
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    title = models.CharField(max_length= 255)
+    picture = models.CharField(max_length=400, blank = True)
+    content = models.TextField()
+    site_url = models.Charfield(max_field=400)
+    vote_total = models.CharField(max_lenth=255)
+
+    def __str__ (self):
+        return self.title
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
