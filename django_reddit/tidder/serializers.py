44b1f220222b0d84a5e4f56614b85e13149a7b88
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-04-12 11:44:12 -0400

    Serializers completed - jordan

--- django_reddit/tidder/serializers.py
+++ django_reddit/tidder/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 
 from .models import Post, Comment
 
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ('created_at', 'content', 'vote_total', 'post')
-
 class PostSerializer(serializers.ModelSerializer):
-    songs = SongSerializer(many=True, read_only=True)
     class Meta:
         model = Post
         fields = ('created_at', 'title picture', 'content', 'site_url', 'vote_total')
+        
+class CommentSerializer(serializers.ModelSerializer):
+    post = PostSerializer(many=False, read_only=True)
+    class Meta:
+        model = Comment
+        fields = ('created_at', 'content', 'vote_total', 'post')
